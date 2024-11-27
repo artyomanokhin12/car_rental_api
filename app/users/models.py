fd741d9ed9ec955d343e82560089c38e0d77cf05
Artyom Anokhin <dudos1515@gmail.com>
2024-11-27 20:50:54 +0300

    Adding users model

---
+++ app/users/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Users(Base):
+	__tablename__ = 'users'
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	email: Mapped[str]
+	hashed_password: Mapped[bytes]
+	first_name: Mapped[str]
+	last_name: Mapped[str]
