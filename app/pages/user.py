17c9046bb9fa4225b3d0d94d4c3ca44a26453b2a
Artyom Anokhin <dudos1515@gmail.com>
2024-12-08 17:20:44 +0300

    Transfer css code to the base template and create a page to display the user page

--- app/pages/user.py
+++ app/pages/user.py
@@ -1,20 +1,37 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from app.users.models import Users
+from app.users.router import get_current_user
 
 
 router = APIRouter(
-    prefix='/user',
     tags=['Фронтэнд Пользователя']
 )
 
-templates = Jinja2Templates(directory="app/templates/users")
+templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get('/login')
+@router.get('/login', response_class=HTMLResponse)
 async def get_hotels_page(
 	request: Request  
 ):
     return templates.TemplateResponse(
-        name='login.html', 
+        name='users/login.html', 
         context={"request": request}
         )
+
+
+@router.get('/me', response_class=HTMLResponse)
+async def get_user(
+    request: Request,
+    user = Depends(get_current_user)
+):
+    return templates.TemplateResponse(
+        name='users/get_user.html',
+        context={
+            'request': request,
+            'user': user,
+		}
+	)
