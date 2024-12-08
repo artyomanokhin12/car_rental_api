4c376b8e2aa404df7ce780efcbdd0f460156618e
Artyom Anokhin <dudos1515@gmail.com>
2024-12-08 17:19:07 +0300

    Add middlewares and cache

--- app/cars/router.py
+++ app/cars/router.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime, timedelta
 from typing import Annotated, List
 from fastapi import APIRouter, Query
+from fastapi_cache.decorator import cache
 
 from app.cars.models import Cars, Category
 from app.cars.queries import CarsQueries
@@ -14,6 +15,7 @@ router = APIRouter(
 
 
 @router.get('')
+@cache(expire=30)
 async def get_all_cars(
 	data: CarsRequest = Query()
 ) -> list[CarsResponse]:
@@ -22,6 +24,7 @@ async def get_all_cars(
 
 
 @router.get('/{id}')
+@cache(expire=360)
 async def get_car_info_by_id(id: int) -> CarsResponse:
 	car = await CarsQueries.find_by_id(id)
 	return car
--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
 
 from app.bookings.router import router as bookings_router
 from app.cars.router import router as cars_router
@@ -7,13 +13,33 @@ from app.reviews.router import router as reviews_router
 from app.users.router import router as users_router
 from app.pages.user import router as page_user_router
 
-api = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	redis = aioredis.from_url('redis://localhost:6379', encoding='utf-8', decode_responses=True)
+	FastAPICache.init(RedisBackend(redis), prefix='cache')
+	yield
+
+
+api = FastAPI(lifespan=lifespan)
 
 api.include_router(router=cars_router)
 api.include_router(router=bookings_router)
 api.include_router(router=reviews_router)
 api.include_router(router=users_router)
+
 api.include_router(router=page_user_router)
 
-
 api.mount("/static", StaticFiles(directory="app/static"), "static")
+
+origins = ['http://localhost:3000']
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", 
+                   "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
