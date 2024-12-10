40a9e66a74421b0c8512bc713a4c3ed2366f099a
Artyom Anokhin <dudos1515@gmail.com>
2024-12-10 11:27:38 +0300

    Adding a router on image uploads and adding a background task execution on photo resolution editing

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ from app.bookings.router import router as bookings_router
 from app.cars.router import router as cars_router
 from app.reviews.router import router as reviews_router
 from app.users.router import router as users_router
+from app.images.router import router as image_router
 from app.pages.user import router as page_user_router
 
 
@@ -26,6 +27,7 @@ api = FastAPI(lifespan=lifespan)
 api.include_router(router=cars_router)
 api.include_router(router=bookings_router)
 api.include_router(router=reviews_router)
+api.include_router(router=image_router)
 api.include_router(router=users_router)
 
 api.include_router(router=page_user_router)
